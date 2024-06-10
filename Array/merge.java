91dce6cd35c64453e38d6a661de706250cbc88b0
Renu Parameswaran <renu.parameswaran@gmail.com>
2024-06-10 14:12:44 -0400

    modify file

--- Array/merge.java
+++ Array/merge.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 class Solution {
     public static int[] merge(int[] nums1, int m, int[] nums2, int n){
-        int p1 = m- 1, p2 = n-1;
+        int p1 = m - 1, p2 = n -1;
         for(int p = m + n - 1; p >= 0; p--){
             if(p2 < 0)
             break;
