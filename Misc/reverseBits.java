0d1926ae1bdfcf5488cd64044bcf18b8ab7a74f5
Renu Parameswaran <renu.parameswaran@gmail.com>
2024-09-18 21:47:38 -0400

    reverse bits problem

---
+++ Misc/reverseBits.java
@@ -0,0 +1,16 @@
+public class reverseBits {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i = 0; i < 32; i++){
+            result <<= 1;
+            result |= (n&1);
+            n >>= 1;
+        }
+        return result;
+    }
+}
+
+
+// TC: O(1)
+// SC: O(1)
