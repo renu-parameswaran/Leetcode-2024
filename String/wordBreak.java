5aa100ea8f1952a587abd4e26d354a0b4b251a12
Renu Parameswaran <renu.parameswaran@gmail.com>
2024-10-16 17:36:34 -0400

    word break problem

---
+++ String/wordBreak.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] && words.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
+
+// TC: O(n^3 + m * k)
+// SC: O(n + m * k)
