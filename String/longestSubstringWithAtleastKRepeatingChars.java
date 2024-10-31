e1d9b03f8c99e5e1b435df704cade83d6acb4202
Renu Parameswaran <renu.parameswaran@gmail.com>
2024-10-30 20:16:02 -0400

    longest substring with atleast k repeating charaters

---
+++ String/longestSubstringWithAtleastKRepeatingChars.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        return longestSubstringUtil(s, 0, s.length(), k);
+    }
+    
+    int longestSubstringUtil(String s, int start, int end, int k){
+        if(end < k) return 0;
+        int[] countMap = new int[26];
+        for(int i = start; i < end; i++){
+            countMap[s.charAt(i) - 'a']++;
+        }
+        for(int mid = start; mid < end; mid++){
+                if(countMap[s.charAt(mid) - 'a'] >= k) continue;
+                int midNext = mid + 1;
+                while(midNext < end && countMap[s.charAt(midNext) - 'a'] < k){
+                    midNext++;
+                }
+                return Math.max(longestSubstringUtil(s, start, mid, k), longestSubstringUtil(s, midNext, end, k));
+                }
+        return (end - start);
+    }
+}
+
+// TC: O(N^2)
+// SC: O(N)
