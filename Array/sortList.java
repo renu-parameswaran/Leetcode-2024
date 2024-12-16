cd8ea8db7c91dfbb2ae7b227a7efbaef08ee1f24
Renu Parameswaran <renu.parameswaran@gmail.com>
2024-12-16 18:43:41 -0500

    sort list

---
+++ Array/sortList.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    ListNode merge(ListNode list1, ListNode list2){
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+
+    ListNode getMid(ListNode head){
+       ListNode slow = head, fast = head, pre = null;
+       while(fast != null && fast.next != null){
+        pre = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+       }
+       pre.next = null;
+       return slow;
+    }
+}
+
+// TC: O(nlogn)
+// SC: O(logn)
